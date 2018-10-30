4864c7553e7b0afbc95728d1ebccd905aa950b5f
Fsned <Fsnedevind@hotmail.com>
2018-10-30 15:30:53 +0100

    Fixxed tons in terminal_node

--- include/terminal_node.h
+++ include/terminal_node.h
@@ -1,18 +1,92 @@
+/* -----------------------------------------------------------------------
+ *                     -------  Functions  -------
+ * ----------------------------------------------------------------------- */
+void commanderFeedbackCallback (const bitten::feedback_msg::ConstPtr& commanderFeedbackMsg);
 
-void help_function();
-void function_1();
-void function_2();
 
 
+bool help_func();
+bool mode_func();
+bool play_test_func();
+bool delete_test_func();
+bool record_func();
+bool get_func();
+bool clearScreen();
 
+int getNumberOfTests();
+void readExistingTests();
+
+
+/* -----------------------------------------------------------------------
+ *                    -------  Enumerations  -------
+ * ----------------------------------------------------------------------- */
 enum KEYWORD_INDICES {
     HELP,
-    GO_TO_MANUAL,
-    GO_TO_WAYPOINT,
-    GO_TO_NONE,
-    PLAY_WP,
-    DELETE_WP,
+    MODE,
+    PLAY_TEST,
+    DELETE_TEST,
+    RECORD,
+    GET,
+    CLEAR,
+
     
 
     NUMBER_OF_KEYWORDS
 };
+
+//     /* 1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768 */
+//     /* 65536, 131072, 262144, 524288, 1048576, 2097152, 4194304 8388608, 16777216  */
+//     ACK                     = 1,    
+//     DENIED                  = 2,
+//     WAYPOINT_REACHED        = 4,
+//     WAYPOINT_INTERRUPTED    = 8,
+//     TEST_DONE_FLAG          = 16,
+//     TEST_INTERRUPTED        = 32,
+//     ROBOT_CRASHED           = 64,
+//     DEADMAN_STATUS          = 128,
+//     ESTABLISH_CONNECTION    = 256,
+//     TERMINATE_CONNECTION    = 512,
+//     PING                    = 1024,
+//     PONG                    = 2048,
+//     PAUSE                   = 4096,
+//     RESUME                  = 8192,
+//     START_OPERATION         = 16384,
+//     STOP_OPERATION          = 32786,
+//     NEW_WAYPOINT            = 65536,
+//     GOAL_REACHED            = 131072,
+enum TERMINAL_FLAGS {
+//     /* 1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768 */
+//     /* 65536, 131072, 262144, 524288, 1048576, 2097152, 4194304 8388608, 16777216  */
+    MODE_MANUAL_F       = 1,
+    MODE_WAYPOINT_F     = 2,
+    MODE_NONE_F         = 4,
+    GET_POSITION_F      = 8,
+    PLAY_TEST_F         = 16,
+    DELETE_TEST_F       = 32,
+    RECORD_TEST_F       = 64,
+    START_RECORD        = 128,
+
+
+};
+
+/* -----------------------------------------------------------------------
+ *                      -------  Variables  -------
+ * ----------------------------------------------------------------------- */
+std::string KeywordStrings[NUMBER_OF_KEYWORDS] = {  "help",
+                                                    "mode",
+                                                    "play_test",
+                                                    "delete_test",
+                                                    "record",
+                                                    "get",
+                                                    "clear",
+                                                     };
+
+bool (* KeywordFunctions[NUMBER_OF_KEYWORDS])( void ) = {   help_func,
+                                                            mode_func,
+                                                            play_test_func,
+                                                            delete_test_func,
+                                                            record_func,
+                                                            get_func,
+                                                            clearScreen      };
+
+
