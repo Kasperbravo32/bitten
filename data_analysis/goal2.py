1024ec16c03a80417060b70b40ffa56e1f158b6d
Kasperbravo32 <kasperbravo@hotmail.com>
2018-11-14 10:34:18 +0100

    stuff

---
+++ data_analysis/goal2.py
@@ -0,0 +1,112 @@
+import matplotlib.pyplot as plt
+import math
+import numpy as np
+
+def avg_list(mylist):
+    avg = sum(mylist)/len(mylist)
+    return avg
+
+actual_pos = [[]0 for x in range(6)]
+goal_pos = [[]0 for x in range(6)]
+goal_time = [[]0 for x in range(6)]
+deviation = [[]0 for x in range(6)]
+
+number_of_points = 0
+
+test_file = open("logfile.txt")
+for line in test_file:
+    if "GOAL" in line:
+        for i in range(0,5):
+            goal_pos[i].append(float(line.split(" ")[i+1]))
+            goal_time[i].append(number_of_points)
+    if "ACTUAL" in line:
+        for i in range(0,5):
+            actual_pos[i].append(float(line.split(" ")[i+1]))
+        number_of_points = number_of_points + 1
+test_file.close()
+
+for i in range(0,5):
+    goal_time[i].pop(0)
+    goal_time[i].append(number_of_points)
+    
+i = 0
+while i < len(goal_pos1):
+    number_at_goal = goal_time1[i]
+    actual_at_goal = actual_pos1[number_at_goal-1]
+    goal_value = goal_pos1[i]
+    deviation = math.degrees(abs(abs(goal_value) - abs(actual_at_goal)))
+    deviation1.append(deviation)
+    i = i + 1
+
+i = 0
+while i < len(goal_pos2):
+    number_at_goal = goal_time2[i]
+    actual_at_goal = actual_pos2[number_at_goal-1]
+    goal_value = goal_pos2[i]
+    deviation = math.degrees(abs(abs(goal_value) - abs(actual_at_goal)))
+    deviation2.append(deviation)
+    i = i + 1
+
+i = 0
+while i < len(goal_pos3):
+    number_at_goal = goal_time3[i]
+    actual_at_goal = actual_pos3[number_at_goal-1]
+    goal_value = goal_pos3[i]
+    deviation = math.degrees(abs(abs(goal_value) - abs(actual_at_goal)))
+    deviation3.append(deviation)
+    i = i + 1
+
+i = 0
+while i < len(goal_pos4):
+    number_at_goal = goal_time4[i]
+    actual_at_goal = actual_pos4[number_at_goal-1]
+    goal_value = goal_pos4[i]
+    deviation = math.degrees(abs(abs(goal_value) - abs(actual_at_goal)))
+    deviation4.append(deviation)
+    i = i + 1
+
+i = 0
+while i < len(goal_pos5):
+    number_at_goal = goal_time5[i]
+    actual_at_goal = actual_pos5[number_at_goal-1]
+    goal_value = goal_pos5[i]
+    deviation = math.degrees(abs(abs(goal_value) - abs(actual_at_goal)))
+    deviation5.append(deviation)
+    i = i + 1
+
+i = 0
+while i < len(goal_pos6):
+    number_at_goal = goal_time6[i]
+    actual_at_goal = actual_pos6[number_at_goal-1]
+    goal_value = goal_pos6[i]
+    deviation = math.degrees(abs(abs(goal_value) - abs(actual_at_goal)))
+    deviation6.append(deviation)
+    i = i + 1
+
+for i in range(0,5):
+    average_deviation[i] = avg_list(deviation[i])
+
+print(average_deviation1)
+print(average_deviation2)
+print(average_deviation3)
+print(average_deviation4)
+print(average_deviation5)
+print(average_deviation6)
+
+average_deviations = []
+average_deviations.append(average_deviation1)
+average_deviations.append(average_deviation2)
+average_deviations.append(average_deviation3)
+average_deviations.append(average_deviation4)
+average_deviations.append(average_deviation5)
+average_deviations.append(average_deviation6)
+
+x = np.arange(6)
+
+plt.figure(1)
+plt.bar(x, average_deviations)
+plt.title("Joint deviations in degrees")
+plt.xticks(x, ('Joint 1', 'Joint 2', 'Joint 3', 'Joint 4', 'Joint 5', 'Joint 6'))
+plt.ylabel("Degrees")
+
+plt.show()
