988f560b1a349daed8489bbbb7683e9af639ae29
Kasperbravo32 <kasperbravo@hotmail.com>
2018-12-05 15:08:28 +0100

    Pictures

--- data_analysis/can_data.py
+++ data_analysis/can_data.py
@@ -14,7 +14,7 @@ can_temp = []
 can_gyro_vert = []
 can_angle = []
 
-can_file = open("DiggingTestOnRobot.log")
+can_file = open("DiggingTestOnExcavator.log")
 # can_id can be 600, 608, 610, 618, 620 or 628
 # Pitch / Roll: 600
 # Boom 1 : 608
@@ -24,7 +24,7 @@ can_file = open("DiggingTestOnRobot.log")
 # Tilt   : 628
 # Robot Sensor is 628... maybe :>
 
-can_id = "628"
+can_id = "608"
 can_id = can_id + "#"
 
 for line in can_file:
@@ -71,29 +71,35 @@ number_of_samples = len(avg_acc_x)
 for i in range(0, number_of_samples):
     angle = math.atan2(avg_acc_y[i], avg_acc_x[i])
     angle = math.degrees(angle) + 90
-    # if angle > 90:
-    #     angle = angle - 180
-    # elif angle < -90:
-    #     angle = angle + 180
+    if angle > 90:
+        angle = angle - 180
+    elif angle < -90:
+        angle = angle + 180
     can_angle.append(angle)
 
 plt.figure(1)
 plt.subplot(311)
 plt.grid(True)
 plt.plot(avg_acc_x, color = "black")
-plt.title("X-accelerometer [g]")
+# plt.title("X-accelerometer [g]")
+plt.xlabel("Samples [n]")
+plt.ylabel("X-acceleration [g]")
 # plt.ylim(-0.095, -0.03)
 
 plt.subplot(312)
 plt.grid(True)
 plt.plot(avg_acc_y, color = "black")
-plt.title("Y-accelerometer [g]")
+# plt.title("Y-accelerometer [g]")
+plt.xlabel("Samples [n]")
+plt.ylabel("Y-acceleration [g]")
 # plt.ylim(0.000, 0.045)
 
 plt.subplot(313)
 plt.grid(True)
 plt.plot(can_angle, color = "black")
-plt.title("Angle [degrees]")
+# plt.title("Angle [degrees]")
+plt.xlabel("Samples [n]")
+plt.ylabel("Angle [degrees]")
 # plt.ylim(-40, 5)
 
 plt.tight_layout(pad=0, w_pad=0, h_pad=-0.6)
